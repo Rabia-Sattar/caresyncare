010685aad8383e69976d4b40b5d91b6a82211652
Rabia-Sattar <153583339+Rabia-Sattar@users.noreply.github.com>
2026-06-09 23:19:49 +0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -2,7 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io(
   // "https://caresync-care-production.up.railway.app" ,
-  "http://localhost:5000",
+  // "http://localhost:5000",
+  "https://caresync-final-year-project-software-engineering-production.up.railway.app",
    {
   transports: ["websocket"], // ✅ force websocket only
   withCredentials: true
