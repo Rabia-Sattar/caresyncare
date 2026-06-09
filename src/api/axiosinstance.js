66867a596a294fb3a75be069a5a147c0e4a4897c
Rabia-Sattar <153583339+Rabia-Sattar@users.noreply.github.com>
2026-06-09 23:21:04 +0500

    Update axiosinstance.js

--- src/api/axiosinstance.js
+++ src/api/axiosinstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "https://caresync-final-year-project-software-engineering-production.up.railway.app",
+  // "http://localhost:5000",
   // "https://caresync-care-production.up.railway.app",
   
 });
